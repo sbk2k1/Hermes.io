fda5f2e5721389141f641cb0cad9d8eba53643fb
sbk2k1 <bhattacharyasaptarshi2001@gmail.com>
2021-11-03 19:04:12 +0530

    Added Geolocation feature

--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
-const path= require('path')
-const http= require('http')
-const express= require('express')
-const socketio= require('socket.io')
-
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
 
 const app = express()
-const server= http.createServer(app)
-const io= socketio(server)
+const server = http.createServer(app)
+const io = socketio(server)
 
-const port= process.env.PORT || 3000
-const publicDirectoryPath= path.join(__dirname, '../public')
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
-
-let count= 0
 
 io.on('connection', (socket) => {
     console.log('New WebSocket connection')
 
-    socket.emit('message', 'Welcome')
-    socket.braodcast.emit('message', 'A new user has joined')
+    socket.emit('message', 'Welcome!')
+    socket.broadcast.emit('message', 'A new user has joined!')
 
     socket.on('sendMessage', (message) => {
         io.emit('message', message)
+    })
+
+    socket.on('sendLocation', (coords) => {
+        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
     })
 
     socket.on('disconnect', () => {
@@ -30,8 +31,6 @@ io.on('connection', (socket) => {
     })
 })
 
-
-
 server.listen(port, () => {
-    console.log(`Server is up on port ${port} !`)
+    console.log(`Server is up on port ${port}!`)
 })
