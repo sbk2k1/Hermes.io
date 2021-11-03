c5606a5f83f6698459f27c977e98fb1aeef7f9ae
sbk2k1 <bhattacharyasaptarshi2001@gmail.com>
2021-11-03 22:05:40 +0530

    Rendering messages to UI

--- public/js/chat.js
+++ public/js/chat.js
@@ -7,8 +7,18 @@ const $messageFormButton= $messageForm.querySelector('button')
 
 const $sendLocationButton= document.querySelector('#send-location')
 
+const $messages= document.querySelector('#messages')
+
+//templates
+const messageTemplate= document.querySelector('#message-template').innerHTML
+
+
 socket.on('message', (message) => {
     console.log(message)
+    const html = Mustache.render(messageTemplate, {
+        message
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
 })
 
 $messageForm.addEventListener('submit', (e) => {
