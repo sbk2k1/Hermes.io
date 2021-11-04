f59bbef358b6525281b7a80c82f8982c39cfc37d
sbk2k1 <bhattacharyasaptarshi2001@gmail.com>
2021-11-04 22:15:28 +0530

    Admin and User messaging

--- src/index.js
+++ src/index.js
@@ -19,43 +19,44 @@ io.on('connection', (socket) => {
     console.log('New WebSocket connection')
 
     socket.on('join', (options, callback) => {
-        const {error, user}= addUser({ id: socket.id, ...options })
+        const { error, user } = addUser({ id: socket.id, ...options })
 
-        if(error) {
+        if (error) {
             return callback(error)
         }
 
         socket.join(user.room)
 
-        socket.emit('message', generateMessage('Welcome'))
-        socket.broadcast.to(user.room).emit('message', generateMessage(`A wild ${user.username} has appeared!`))
+        socket.emit('message', generateMessage('Admin', 'Welcome'))
+        socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `A wild ${user.username} has appeared!`))
 
         callback()
     })
 
     socket.on('sendMessage', (message, callback) => {
-
+        const user = getUser(socket.id)
         const filter = new Filter()
 
         if (filter.isProfane(message)) {
             return callback('Profanity is not allowed!')
         }
 
-        io.emit('message', generateMessage(message))
+        io.to(user.room).emit('message', generateMessage(user.username, message))
         callback()
     })
 
     socket.on('sendLocation', (coords, callback) => {
-        io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
+        const user = getUser(socket.id)
+        io.to(user.room).emit('locationMessage', generateLocationMessage(user.username, `https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
 
         callback()
     })
 
     socket.on('disconnect', () => {
-        const user= removeUser(socket.id)
+        const user = removeUser(socket.id)
 
         if (user) {
-            io.to(user.room).emit('message', generateMessage(`${user.username} has to return some videotapes!`))
+            io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has to return some videotapes!`))
         }
     })
 })
