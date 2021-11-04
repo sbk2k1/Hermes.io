ebbd04da7dca2383a9e4beaff9a8b4466b19fdac
sbk2k1 <bhattacharyasaptarshi2001@gmail.com>
2021-11-04 18:26:33 +0530

    CRUD for Users

---
+++ src/utils/users.js
@@ -0,0 +1,41 @@
+const users = []
+
+//addUser removeUser getUser getUsersInRoom
+
+const addUser = ({ id, username, room }) => {
+    //clean the data
+    username= username.trim().toLowerCase()
+    room= room.trim().toLowerCase()
+
+    //validate tha data
+    if(!username || !room) {
+        return {
+            error: 'Username and room are required'
+        }
+    }
+
+    //check for existing user 
+    const existingUser = users.find((user) => {
+        return user.room === room && user.username=== username
+    })
+
+    //validate username
+    if(existingUser){
+        return {
+            error: 'Username is in use'
+        }
+    }
+
+    //store user
+    const user= { id, username, room }
+    users.push(user)
+    return { user }
+}
+
+const removeUser = (id) => {
+    const index = users.findIndex((user) => user.id===id)
+
+    if(index !== -1){
+        return users.splice(index, 1)[0]
+    }
+}
